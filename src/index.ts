451a86ec6cd2e50b19b631939cadcef5fca98d81
gilboom <linxiji@tuzhanai.com>
2020-03-28 23:40:44 +0800

    feat: index.ts
    修改响应

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { createServer } from "http";
 const config = require("../config.json");
 const server = createServer((req, res) => {
   console.log(`${req.method.toUpperCase()} ${req.url}`)
-  res.end("Hello World");
+  res.end("Hello 靓仔吉");
 });
 const port = config.port;
 server.listen(port, () => {
